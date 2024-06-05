9407f848f2d0550c4bae5cf2902bae245f637c9a
AlamRidha <alamrridha@gmail.com>
2024-06-05 20:32:15 +0700

    create update delete data

--- routes/users.js
+++ routes/users.js
@@ -30,10 +30,69 @@ router.post("/", async (req, res, next) => {
 });
 
 // update data
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
 
-router.get("/test", (req, res) => {
+  let user = await Users.findByPk(id);
+
+  // cek user di db
+  if (!user) {
+    return res.status(400).json({ msg: "User tidak ditemukan" });
+  }
+
+  const schema = {
+    nip: "string|optional",
+    nameUser: "string|optional",
+    password: "string|min: 5|optional",
+    email: "string|optional",
+    noHp: "string|max:13|optional",
+    bidangMataPelajaran: "string|optional",
+  };
+
+  const validate = v.validate(req.body, schema);
+  // cek validasi
+  if (validate.length) {
+    return res.status(400).json(validate);
+  }
+
+  user = await user.update(req.body);
+  res.json(user);
+});
+
+// get all data
+router.get("/", async (req, res) => {
+  const users = await Users.findAll();
+
+  return res.json(users);
+});
+
+// get data by id
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await Users.findByPk(id);
+
+  // // cek user di db
+  // if (!user) {
+  //   return res.status(400).json({ msg: "User tidak ditemukan" });
+  // }
+
+  return res.json(user || {});
+});
+
+// delete user
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await Users.findByPk(id);
+
+  // cek user di db
+  if (!user) {
+    return res.status(400).json({ msg: "User tidak ditemukan" });
+  }
+
+  await user.destroy();
+
   res.json({
-    msg: "Hello ini dunia",
+    msg: "user berhasil dihapus",
   });
 });
 
